c9146b7dfd7c678fc6e0708a6b879259897081b6
kevin cardoso <59876891+KevinMdCardoso@users.noreply.github.com>
2020-12-02 23:50:00 -0300

    Crud

--- headquarters-airsoft-app/Controllers/JogadorController.cs
+++ headquarters-airsoft-app/Controllers/JogadorController.cs
@@ -19,41 +19,78 @@ namespace EFCore.WebAPI.Controllers
             _context = context;
         }
 
-        // GET api/values/5
-        //[HttpGet("{nameJogador}")]
-        //public ActionResult Get(string nameJogador)
-        //{
-        //    var jogador = new Jogador { Nome = nameJogador };
-
-        //    _context.Jogadores.Add(jogador);
-        //    _context.SaveChanges();
-
-        //    return Ok();
-        //}
-
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
-            var jogador = new Jogador();
-            jogador = _context.Jogadores.Find(id);
+        
+            var jogador = _context.Jogadores.Find(id);
             return Ok(jogador);
+        }
+
+        [HttpPost]
+        public JogadorViewModel Post([FromBody] JogadorPayload payload)
+        {
+            JogadorViewModel viewModel = new JogadorViewModel();
+            var novoJogador = new Jogador();
+            novoJogador.CPF = payload.CPF;
+            novoJogador.Email = payload.Email;
+            novoJogador.Nome = payload.Nome;
+            //jogador.Senha = payload.Senha; 
+            viewModel.Data = novoJogador;
+
+            _context.Jogadores.Add(novoJogador);
+            _context.SaveChanges();           
+
+            return viewModel;
+        }
+
+        [HttpPut("{id}")]
+        public JogadorViewModel Put(int id, [FromBody] JogadorPayload payload)
+        {
+            JogadorViewModel viewModel = new JogadorViewModel();
+
+            var jogador = _context.Jogadores.Find(id);
+            jogador.CPF = payload.CPF;
+            jogador.Email = payload.Email;
+            jogador.Nome = payload.Nome;
+            //jogador.Senha = payload.Senha;            
+            viewModel.Data = jogador;
+
+            _context.SaveChanges();
+
+            return viewModel;
         }
 
         // DELETE
         [HttpDelete("{id}")]
-        public void Delete(int id) 
+        public ActionResult Delete(int id) 
         {
             var jogador = _context.Jogadores.Where(x => x.Id == id).Single();
 
             _context.Jogadores.Remove(jogador);
             _context.SaveChanges();
+            return Ok("Jogador Deletado");
         }
 
         [HttpGet]
         public ActionResult Get()
-        {
-            return Ok("Olá mundo!");
+        {         
+            var jogadores = _context.Jogadores.ToList();
+            return Ok(jogadores);
         }
 
+    }
+
+    public class JogadorPayload
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+
+    public class JogadorViewModel
+    {
+        public Jogador Data{ get; set; }
     }
 }
